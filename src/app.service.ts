e2a1617872b6f45c4fcafe4252a6b27383145dea
Tuan Le <tlna95@icloud.com>
2023-08-03 02:54:32 -0400

    add GitHub API back

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { Octokit } from '@octokit/core';
 
 @Injectable()
 export class AppService {
+  octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
   getIndex(): string {
     return '<h1>Portfolio Back-end</h1><p>Nguyen Anh Tuan Le</p>';
+  }
+
+  getCurrentGitHubUser(): Promise<JSON> {
+    return new Promise<JSON>((resolve, reject) => {
+      this.octokit
+        .request('GET /user')
+        .then((result) => {
+          resolve(result.data as any);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  getGitHubReposBasedOnCurrentUser(): Promise<JSON> {
+    return new Promise((resolve, reject) => {
+      this.octokit
+        .request('GET /user/repos', {
+          affiliation: 'owner',
+          sort: 'updated',
+          per_page: 333,
+        })
+        .then((result) => {
+          resolve(result.data as any);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   }
 }
